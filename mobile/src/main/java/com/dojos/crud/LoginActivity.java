8fba49e449209f088be30acaea2340368b6663f4
Thiago Barz <thiagobarz@cit>
2016-03-24 11:02:23 -0300

    Versão parecida com o DOJO 2

--- mobile/src/main/java/com/dojos/crud/LoginActivity.java
+++ mobile/src/main/java/com/dojos/crud/LoginActivity.java
@@ -5,17 +5,42 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText usuario;
+    private EditText senha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        usuario = ((EditText) findViewById(R.id.usuario));
+        senha = ((EditText) findViewById(R.id.senha));
     }
 
-    public void loginClick(View view){
-        EditText password = ((EditText)findViewById(R.id.password));
-        Intent intent = new Intent(this, GeralActivity.class);
+    /**
+     * Valida se os campos foram preenchidos e abre a tela principal.
+     *
+     * @param view
+     */
+    public void realizarLogin(View view) {
+        if (isCamposPreenchidos()) {
+            Toast.makeText(this, getString(R.string.toastPreenchaUsuarioSenha), Toast.LENGTH_LONG);
+        } else {
+            Intent intentTelaGeral = new Intent(this, GeralActivity.class);
+            startActivity(intentTelaGeral);
+        }
+    }
+
+    /**
+     * Verifica se os campos obrigatórios foram preenchidos.
+     *
+     * @return TRUE se foram preenchidos e FALSE em caso contrário.
+     */
+    private Boolean isCamposPreenchidos() {
+        return (!usuario.getText().toString().isEmpty() && !senha.getText().toString().isEmpty());
     }
 }
